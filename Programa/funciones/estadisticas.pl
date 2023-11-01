b0d3df3f8b58faca2d73ba2a0a1c842ac8bcd6f1
Duan Antonio Espinoza Olivares <75913012+Duan-Espinoza@users.noreply.github.com>
2023-11-01 01:42:57 -0600

    mejora estadistica
    se prueba otra versión

--- Programa/funciones/estadisticas.pl
+++ Programa/funciones/estadisticas.pl
@@ -1,47 +1,16 @@
-:- dynamic proyecto/5.  % Definir dinámicamente un hecho para representar cada proyecto.
-:- dynamic tarea/4.     % Definir dinámicamente un hecho para representar cada tarea.
+% :- dynamic proyecto/5. % Definir dinámicamente un hecho para representar cada proyecto.
+% :- dynamic tarea/4. % Definir dinámicamente un hecho para representar cada tarea.
 
 % Funcionalidad de estadisticas
 % Duan Antonio Espinoza
 % Proyecto 3
 
-
-
-% Cargar proyectos desde el archivo proyectos.txt
-cargar_proyectos :-
-    open('../data/proyectos.txt', read, Stream),
-    cargar_proyectos_desde_archivo(Stream),
-    close(Stream).
-
-% Cargar tareas desde el archivo tareas.txt
-cargar_tareas :-
-    open('../data/tareas.txt', read, Stream),
-    cargar_tareas_desde_archivo(Stream),
-    close(Stream).
-
-
-% Predicado para cargar proyectos desde un archivo
-cargar_proyectos_desde_archivo(Stream) :-
-    repeat,
-    read(Stream, Proyecto),
-    (   Proyecto == end_of_file -> ! ;
-        assert(Proyecto), fail
-    ).
-
-% Predicado para cargar tareas desde un archivo
-cargar_tareas_desde_archivo(Stream) :-
-    repeat,
-    read(Stream, Tarea),
-    (   Tarea == end_of_file -> ! ;
-        assert(Tarea), fail
-    ).
-
-
+% Se cargan automáticamente los proyectos y tareas desde los archivos
+:- [gestionProyectos].
+:- [gestionTareas].
 
 % Estatus financiero de un proyecto
 estatus_financiero(NombreProyecto, Estatus) :-
-    cargar_proyectos,  % Cargar proyectos antes de calcular el estatus financiero
-    cargar_tareas,    % Cargar tareas antes de calcular el estatus financiero
     findall(Costo, tarea(NombreProyecto, _, _, Costo), Costos),
     sum_list(Costos, TotalCostoTareas),
     proyecto(NombreProyecto, _, Presupuesto, _, _),
@@ -51,8 +20,6 @@ estatus_financiero(NombreProyecto, Estatus) :-
 
 % Calcular estatus financiero para todos los proyectos
 calcular_estatus_financiero_proyectos :-
-    cargar_proyectos,  % Cargar proyectos antes de calcular el estatus financiero
-    cargar_tareas,    % Cargar tareas antes de calcular el estatus financiero
     findall(NombreProyecto, proyecto(NombreProyecto, _, _, _, _), Proyectos),
     calcular_estatus_financiero_proyectos(Proyectos).
 
@@ -62,19 +29,16 @@ calcular_estatus_financiero_proyectos([NombreProyecto|RestoProyectos]) :-
     write('Proyecto: '), write(NombreProyecto), write(', Estatus financiero: '), write(Estatus), nl,
     calcular_estatus_financiero_proyectos(RestoProyectos).
 
-
 % Tipo de cierre de un proyecto
 tipo_cierre_proyecto(NombreProyecto, Tipo) :-
     proyecto(NombreProyecto, _, _, FechaCierre, _),
-    findall(FechaMaximaCierreTarea, tarea(NombreProyecto, _, _, _, FechaMaximaCierreTarea), FechasMaximas),
+    findall(FechaMaximaCierreTarea, tarea(NombreProyecto, _, _, FechaMaximaCierreTarea), FechasMaximas),
     max_list(FechasMaximas, MaxFechaMaximaCierre),
     (FechaCierre >= MaxFechaMaximaCierre -> Tipo = 'Al día';
      FechaCierre < MaxFechaMaximaCierre -> Tipo = 'Tardía').
 
 % Calcular tipo de cierre para todos los proyectos
 calcular_tipo_cierre_proyectos :-
-    cargar_proyectos,  % Cargar proyectos antes de calcular el estatus financiero
-    cargar_tareas,    % Cargar tareas antes de calcular el estatus financiero
     findall(NombreProyecto, proyecto(NombreProyecto, _, _, _, _), Proyectos),
     calcular_tipo_cierre_proyectos(Proyectos).
 
@@ -84,13 +48,10 @@ calcular_tipo_cierre_proyectos([NombreProyecto|RestoProyectos]) :-
     write('Proyecto: '), write(NombreProyecto), write(', Tipo de cierre: '), write(Tipo), nl,
     calcular_tipo_cierre_proyectos(RestoProyectos).
 
-
 % Calcular cantidad de tareas por persona
 calcular_cantidad_tareas_por_persona :-
-    cargar_proyectos,  % Cargar proyectos antes de calcular el estatus financiero
-    cargar_tareas,    % Cargar tareas antes de calcular el estatus financiero
     findall(Persona, tarea(_, Persona, _, _), Personas),
-    list_to_set(Personas, PersonasUnicas),  % Elimina duplicados
+    list_to_set(Personas, PersonasUnicas), % Elimina duplicados
     calcular_cantidad_tareas(PersonasUnicas).
 
 calcular_cantidad_tareas([]).
